65996a843d8e7dec912b11ab74418ece0c422c2d
Jorge Casaudoumecq <Jorge@Casaudoumecq.me>
2023-04-11 15:58:44 -0400

    Installed many packages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
+import HomePage from './Pages/HomePage/HomePage';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+
+
+        </Routes>
+      </BrowserRouter>
 
     </div>
   );
